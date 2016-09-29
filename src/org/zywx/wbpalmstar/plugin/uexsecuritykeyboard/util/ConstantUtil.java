4129b035f0afe79b39e35af6a597db40b026aaa0
autowanglei <automationwang@126.com>
2016-09-29 17:59:33 +0800

    open接口新增showInputBox参数，配置插件是否显示输入框；添加onKeyPress监听

---
+++ src/org/zywx/wbpalmstar/plugin/uexsecuritykeyboard/util/ConstantUtil.java
@@ -0,0 +1,7 @@
+package org.zywx.wbpalmstar.plugin.uexsecuritykeyboard.util;
+
+public class ConstantUtil {
+    public static final int INPUT_TYPE_TEXT = 0;
+    public static final int INPUT_TYPE_DEL = 1;
+    public static final int INPUT_TYPE_DONE = 2;
+}
